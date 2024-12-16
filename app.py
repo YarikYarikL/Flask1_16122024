0322768c1cd1ee341cd7317e1bef27042fca8532
YarikYarikL <yariklaz@yandex.ru>
2024-12-16 14:45:33 +0300

    Add ABOUT url

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 
-@app.route("/")
-def hello_world():
+about_me = {
+"name": "Евгений",
+"surname": "Юрченко",
+"email": "eyurchenko@specialist.ru"
+}
+
+
+@app.route("/") #первый url, который мы обрабатываем
+def hello_world():#функций-обработчик
     return "Hello, World!"
+
+
+@app.route("/about")
+def about():
+    return about_me
 
 
 if __name__ == "__main__":
