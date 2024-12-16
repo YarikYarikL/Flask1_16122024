b719832f5020c8e02f2be37c98b772f2c9d6624a
YarikYarikL <yariklaz@yandex.ru>
2024-12-16 16:12:36 +0300

    Done. M1:part1 task1-2

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, jsonify, Response
+from typing import Any
+from markupsafe import escape
+
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -11,6 +14,39 @@ about_me = {
 }
 
 
+quotes = [
+{
+"id": 1,
+"author": "Rick Cook",
+"text": "Программирование сегодня — это гонка \
+разработчиков программ, стремящихся писать программы с \
+большей и лучшей идиотоустойчивостью, и вселенной, которая \
+пытается создать больше отборных идиотов. Пока вселенная \
+побеждает."
+},
+{
+"id": 2,
+"author": "Waldi Ravens",
+"text": "Программирование на С похоже на быстрые танцы \
+на только что отполированном полу людей с острыми бритвами в \
+руках."
+},
+{
+"id": 3,
+"author": "Moshers Law of Software Engineering",
+"text": "Не волнуйтесь, если что-то не работает. Если \
+бы всё работало, вас бы уволили."
+},
+{
+"id": 4,
+"author": "Yoggi Berra",
+"text": "В теории, теория и практика неразделимы. На \
+практике это не так."
+},
+]
+
+
+
 @app.route("/") #первый url, который мы обрабатываем
 def hello_world():#функций-обработчик
     return jsonify(hello="Hello, World!"),200
@@ -19,6 +55,26 @@ def hello_world():#функций-обработчик
 @app.route("/about")
 def about():
     return jsonify(about_me),200
+
+
+@app.route("/quotes")
+def get_quotes() -> list[dict[str: Any]]:
+    """ функция преобразует список словарей в массив объектов json"""
+    return jsonify(quotes),200
+
+
+@app.route("/quotes/<int:quote_id>")
+def get_quote(quote_id: int) -> dict:
+    for quote in quotes:
+        if quote.get("id") == quote_id:
+            return jsonify(quote), 201
+    return {"error":f"цитаты с id {quote_id} нет"}, 404
+
+
+@app.route("/quotes/count")
+def count_quote():
+    qoutes_number = len(quotes)
+    return f'всего цитат {escape(qoutes_number)}',200
 
 
 if __name__ == "__main__":
